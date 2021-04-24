b5c26b3500ccd01a65f572cf022470788494fafe
antfromano <antfromano@gmail.com>
2021-04-24 19:09:15 +0000

    added functions to check for match by accessing previously assigned dataset and added freezeGame to prevent card flipping while playing in scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,46 @@
 const cards = document.querySelectorAll('.match-card');
 
 let hasFlippedCard = false;
+let freezeBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip')
+    if (freezeBoard) return;
+    this.classList.toggle('flip');
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return;
     }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+checkForMatch();
 }
 
-if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
+function checkForMatch() {
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.remoteEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    freezeBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        freezeBoard = false;
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
