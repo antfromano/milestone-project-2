61f0e6e168abe138a2538d355f71e58674896ad5
antfromano <antfromano@gmail.com>
2021-04-24 14:36:17 +0000

    added matching logic to manage flip state in script.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,21 @@
 const cards = document.querySelectorAll('.match-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.toggle('flip')
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    }
+}
+
+if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
